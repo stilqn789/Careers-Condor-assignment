bbb4f740ce3da23efafbb4646a2839bbc4cf5f0d
Stilyan Tinkov Markovski <58570022+stilqn789@users.noreply.github.com>
2020-02-22 16:45:56 +0200

    Add files via upload

---
+++ Stilyan Markovski - assignment/index.js
@@ -0,0 +1,9 @@
+function toggle(){
+    var dropdownM = document.getElementById("dropDownMenu" );
+    if (dropdownM.style.display == "none") {
+        dropdownM.style.display = "block";
+    } else {
+        dropdownM.style.display = "none";
+    }        
+}
+
